92f58c17539bf08822a8e1673d2ec66a7574e46b
Jan <jan.marre@udo.edu>
2021-07-22 13:22:31 +0200

    Added Bluetooth URL

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        String board2 = "btspp://98D311FC1B55:1;authenticate=false;encrypt=false;master=false";
+
+        String board1 = "btspp://98D361FD57F8:1;authenticate=false;encrypt=false;master=false";
+
+
         /*Client client1 = new Client(BluetoothScanner.scanForDeviceByIdentifier("HC.*"));
         client1.initClientConnection();
         client1.sentToDevice("Hello World");
