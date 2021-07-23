24b74caec87d7d796077c1c2ceeadcd960e8f545
Jan <jan.marre@udo.edu>
2021-07-23 12:05:27 +0200

    multi Bluetooth test

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         client1.sentToDevice("Hello World");
         client1.readFromDevice();
         client1.closeClientConnection();
-        //lol
+        //lola
         */
 
 
