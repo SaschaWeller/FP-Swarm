05ada74564239171e5d95b536127c76e90e6f2a2
S Weller <weller.sascha@web.de>
2021-09-02 15:26:53 +0200

    Add ML aggragation

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        
+
         //init drones and conections
         String board1 = "btspp://98D361FD57F8:1;authenticate=false;encrypt=false;master=false"; // Jan 1
         String board2 = "btspp://98D311FC1B55:1;authenticate=false;encrypt=false;master=false"; // Jan 2
@@ -25,17 +25,31 @@ public class Main {
         allDrones.add(client3);
         
         //init ML-Algo
-        LinkedList<String> results = new LinkedList<String>();
+        LinkedList<String> droneResults = new LinkedList<String>();
         String newWeights ="";
 
 
         //collect data from all drones
         for (Client client : allDrones){
-            results.add(client.readFromDevice());
+            droneResults.add(client.readFromDevice());
         }
-        
+
         //ML aggregation
 
+        HashMap<String, ArrayList<Integer>> tmpMap = stringsToMap(droneResults);
+        HashMap<String, Object> aggregationResults =new HashMap<>();
+
+        tmpMap.forEach( (k,v) -> {
+            Map<Object, Long> counterMap = v.stream().collect(Collectors.groupingBy(e -> e.toString().toLowerCase(),Collectors.counting()));
+
+            long  maxValueInMap=(Collections.max(counterMap.values()));
+            for (Map.Entry<Object, Long> entry : counterMap.entrySet()) {
+                if (entry.getValue()==maxValueInMap) {
+                    aggregationResults.put(k , entry.getKey());
+                }
+            }
+
+        });
 
 
         //sent new Weights to all drones
@@ -49,7 +63,6 @@ public class Main {
             client.closeClientConnection();
             allDrones.remove(client);
         }
-
 
 
 /*      Here two comlete conections for exampel:
@@ -68,5 +81,25 @@ public class Main {
         client2.closeClientConnection();
 */
 
+    }
+    static HashMap<String, ArrayList<Integer>> stringsToMap(LinkedList<String> input){
+        HashMap<String, ArrayList<Integer>> result = new HashMap<String, ArrayList<Integer>>();
+
+        for( String inputElement : input) {
+            String tmp = inputElement.substring(inputElement.indexOf("/") + 1);
+            tmp = tmp.replace("/", "");
+            String[] segments = tmp.split("\\$");
+
+            for(String segment : segments){
+                //String key = segment.substring(0, segment.lastIndexOf("#")).replace("#", "");
+                String key = segment.substring(0, segment.lastIndexOf("#"));
+                Integer value = Integer.parseInt(segment.substring(segment.lastIndexOf("#") + 1));
+                if (result.get(key) == null){
+                    result.put(key, new ArrayList<Integer>());
+                }
+                result.get(key).add(value);
+            }
+        }
+        return  result;
     }
 }
